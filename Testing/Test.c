10f2cb8776f8036c870b0e8a8dadfb3296c73ac8
29NZafiri <29nzafiri@paduafranciscan.net>
2025-10-07 20:41:00 -0400

    Cleared out junk code in Test

--- Testing/Test.c
+++ Testing/Test.c
@@ -5,10 +5,4 @@
 
 void test(void) {
     printf("Test\n");
-    double x;
-    while (x != 0) {
-        scanf("%lf", &x);
-        double y = 2*sqrt(x*x-9)/3;
-        printf("Y = %.17g\n", y);
-    }
 }
