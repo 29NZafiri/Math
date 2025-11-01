167bcf7b54d440b8dd97e8e4ee1eeb7bb3bf775a
29NZafiri <29nzafiri@paduafranciscan.net>
2025-11-01 14:11:47 -0400

    Fixed Locale Setting

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <locale.h>
 #include <string.h>
 
+#include "Constants.h"
+#include "sscanf.h"
 #include "Geometry/Geometry.h"
 #include "Algebra/Algebra.h"
 #include "Analytical Geometry/Graphing.h"
@@ -21,24 +23,26 @@ int main(int argc, char *argv[]) {
     }
     else {
         printf("Set locale to UTF-8? (Recommended to do so) Y/N\n");
-        int utf;
-        scanf("%c", &utf);
-        if (utf == 'Y' || utf == 'y') {
+        char* utf[BUFFER_SIZE];
+        scanf("%99s", utf);
+        if (strcmp(utf, "Y") == 0 || strcmp(utf, "y") == 0) {
             printf("Setting Locale...\n");
             setlocale(LC_ALL, "en_US.UTF-8");
         }
+        else
+            printf("Proceeding with your Locale...\n");
     }
     while (sub != 0) {
         printf("\nWhat would you like to do?\n");
         printf("Geometry (1)\n"
-            "Stereometry (2)\n"
+            " Stereometry (2)\n"
             "Algebra (3)\n"
             "Analytical Geometry/Graphing (4)\n"
             "Number Theory (5)\n"
             "Counting (6)\n"
             "Misc (7)\n");
         printf("Which one? (0 when done)\n");
-        scanf("%d", &sub);
+        ssscanf("%d", &sub);
         switch (sub) {
             case 1:
                 Geometry();
