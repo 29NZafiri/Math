25014c9bebd81bc35d03270ba9abf56ba26eef15
29NZafiri <29nzafiri@paduafranciscan.net>
2025-11-09 13:49:58 -0500

    Removed reduntant correction

--- Analytical Geometry/RotatePoint.c
+++ Analytical Geometry/RotatePoint.c
@@ -21,7 +21,6 @@ void rtPnt() {
     x -= rx;    //Translate x so the rotation takes place about the origin
     y -= ry;    //Translate y so the rotation takes place about the origin
     double theta = atan2(y, x);     //Covert to Polar Coordinates
-    if (theta < 0) theta += 2 * PI; //Correct atan2 to return [0, 2*pi)
     double r = sqrt(x*x + y*y);     //Covert to Polar Coordinates
     angle *= PI/180;     //Convert to radians
     theta = theta + angle;    //Adding the angles in polar coords gives the coords after rotation
@@ -29,3 +28,4 @@ void rtPnt() {
     double fy = r*sin(theta) + ry;    //Convert to Cartesian Coordinates
     printf("The coordinates after the rotation are (%.15g,%.15g)\n", fx, fy);
 }
+
