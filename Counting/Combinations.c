17daf6f24f056714780ee3d3d09de8b4cbe1d350
29NZafiri <29nzafiri@paduafranciscan.net>
2025-11-21 23:48:27 +0200

    Updated nCr's for loop to be more intuative
    Rather than the loop counting up, it counts down. This is similar to how the factorial is calculated.
    Also replaced scanf with ssscanf

--- Counting/Combinations.c
+++ Counting/Combinations.c
@@ -5,18 +5,16 @@
 void nCr(void) {
     int n, ir, r;
     printf("How many objects? (n in nCr)\n");
-    scanf("%d", &n);
+    ssscanf("%d", &n);
     printf("How many to pick? (r in nCr)\n");
-    scanf("%d", &ir); //input r
+    ssscanf("%d", &ir); //input r
     if (n/2 < ir) r = n-ir;
     else r = ir;
     unsigned long long int num = 1, denom = 1;
     for (unsigned long long int i = 0; i < r; i++)
         num *= n-i;
-    for (unsigned long long int i = 2; i <= r; i++)
+    for (unsigned long long int i = r; i >= 2; i--)
         denom *= i;
     unsigned long long int result = num / denom;
     printf("%dC%d = %lld\n", n, ir, result);
 }
-
-
